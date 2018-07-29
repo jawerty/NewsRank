fd729a9e0be54e5e00118a7e4db24bebbf7aaec9
Jared Wright <jared@jareds-mbp.home>
2018-07-29 19:25:08 -0400

    change

--- StoryPipeline/CredScorer.js
+++ StoryPipeline/CredScorer.js
@@ -242,7 +242,7 @@ function CredScorer(articleMap, trackerList) {
 		self = this;
 		const pubBiasFactData = {};
 		// Get tracker info first
-		this.fetchTrackersForPubs(false, (foundTrackerMap) => {
+		this.fetchTrackersForPubs(true, (foundTrackerMap) => {
 			async.each(Object.keys(foundTrackerMap), (publicationDomain, callback) => {
 				articleModel.findOne({publication: publicationDomain}, {publicationName: 1, publication: 1}, (err, foundArticle) => {
 					if (!err && foundArticle && typeof foundArticle["publicationName"] != "undefined") {
