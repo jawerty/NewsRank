75fc9e662fd8bfbdfa3e82b9dbaddd30572ebfec
Jared Wright <jared@Jareds-MacBook-Pro.local>
2018-08-07 22:23:12 -0400

    add article archiving

---
+++ StoryPipeline/ArchiveArticles.js
@@ -0,0 +1,35 @@
+const db = require('../db/schema');
+const articleModel = db.model('article');
+
+const ArchiveArticles = () => {
+	let d = new Date();
+	const lastWeekTs = d.setDate(d.getDate() - 7);
+	d = new Date();
+	const twoDaysAgoTs = d.setDate(d.getDate() - 2);
+	console.log("Removing articles before "+lastWeekTs);
+	articleModel.remove({date_scrapped: { $lt: lastWeekTs} }, () => {
+		console.log("Archiving articles before "+twoDaysAgoTs);
+		const articleCursor = articleModel.find({
+			date_scrapped: { $lt: twoDaysAgoTs },
+			trained: true
+		}).cursor();
+
+		let i = 0;
+		articleCursor.on('data', (article) => {
+			articleCursor.pause();
+			i++;
+			delete article.content;
+			article.save();
+			articleCursor.resume();
+		});
+
+		articleCursor.on('end', () => {
+			console.log("Archived "+i+" articles");
+			setTimeout(() => {
+				process.exit(0);
+			}, 1000);
+		});
+	});
+}
+
+ArchiveArticles();	
