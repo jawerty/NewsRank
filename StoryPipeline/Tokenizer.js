d4e0e89f771b89afb5cc1d5eb3833364f4f443f7
Jared Wright <jared@jareds-mbp.home>
2018-08-09 03:08:44 -0400

    tokenizer fix

--- StoryPipeline/Tokenizer.js
+++ StoryPipeline/Tokenizer.js
@@ -40,70 +40,92 @@ const Tokenizer = () => {
 	let articleTokens = {};
 	let iterator = 0;
 
-	const articleCursor = articleModel.find({tokens: {$exists: false}}).cursor();
-	articleCursor.on("data", (article) => {
-		articleCursor.pause() // run events as they are added to call stack
-		const $ = cheerio.load(article.content);
-		const allText = $("*").text();
+	// group dates by day [[timestamp, timestamp], [timestamp, timestamp]];
+	const articleCursor = articleModel.count({tokens: {$exists: false}}, (err, count) => {
+		if (count == 0) {
+			return;
+		}
 
-		wordpos.getPOS(allText, (pos) => {
-			let tokens = [];
-			const nouns = pos.nouns;
-			const rest = pos.rest; // all non POS (usually real world indentifiers)
-			nouns.forEach((word) => {
-				tokens.push([word, "noun"]);
+		const iterations = Math.floor(count / 500);
+		let iterationArray = [];
+		for (let i = 0; i < iterations; i++) {
+			iterationArray.push(i);
+		}
+		let amount = 0;
+		async.each(iterationArray, (iteration, callback) => {
+			const articleCursor = articleModel.find({tokens: {$exists: false}}).skip(amount).limit(500).cursor();
+			articleCursor.on("data", (article) => {
+				articleCursor.pause() // run events as they are added to call stack
+				const $ = cheerio.load(article.content);
+				const allText = $("*").text();
+
+				wordpos.getPOS(allText, (pos) => {
+					let tokens = [];
+					const nouns = pos.nouns;
+					const rest = pos.rest; // all non POS (usually real world indentifiers)
+					nouns.forEach((word) => {
+						tokens.push([word, "noun"]);
+					});
+					rest.forEach((word) => {
+						tokens.push([word, "rest"]);
+					});
+					tfidf.addDocument(allText.toLowerCase());
+					console.log("Adding document "+article.title);
+					articleTokens[iterator] = {
+						article,
+						tokens
+					};
+					iterator++;
+					articleCursor.resume(); // get new data as their callbacks are added to call stack
+				});
 			});
-			rest.forEach((word) => {
-				tokens.push([word, "rest"]);
+
+			articleCursor.on('end', () => { 
+				console.log("All documents have been added for tokenizing");
+
+				// meh
+				const articlesCount = Object.keys(articleTokens).length;
+				async.each(Object.keys(articleTokens), (index, topCallback) => {
+					const tokenWeights = {};
+					async.each(articleTokens[index].tokens, (token, bottomCallback) => {
+						const weight = tfidf.tfidf(token[0], index);
+						// if (articleTokens[index].article.title.includes("Israel"))
+						// 	console.log(
+						// 		articleTokens[index].article.title,
+						// 		token,
+						// 		weight
+						// 	);
+
+						if (weight > 70 && token[1] == "noun") tokenWeights[token[0]] = weight
+						else if (weight > 30 && token[1] == "rest") tokenWeights[token[0]] = weight
+						bottomCallback();
+					}, (err) => {
+						let weightedTokens = genWeightedTokens(tokenWeights);
+						weightedTokens = weightedTokens.map((token) => token.toLowerCase());
+						articleTokens[index].article.tokens = weightedTokens;
+						articleTokens[index].article.markModified("tokens");
+						articleTokens[index].article.save();
+
+						topCallback();
+					})
+					
+				}, (err) => {
+					amount += 500;
+					if (amount > count) {
+						amount = count;
+					}
+				    console.log("Processed "+amount+" out of "+count);
+				    callback();
+				});
 			});
-			tfidf.addDocument(allText.toLowerCase());
-			console.log("Adding document "+article.title);
-			articleTokens[iterator] = {
-				article,
-				tokens
-			};
-			iterator++;
-			articleCursor.resume(); // get new data as their callbacks are added to call stack
-		});
-	});
-
-	articleCursor.on('end', () => { 
-		console.log("All documents have been added for tokenizing");
-
-		// meh
-		const articlesCount = Object.keys(articleTokens).length;
-		async.each(Object.keys(articleTokens), (index, topCallback) => {
-			const tokenWeights = {};
-			async.each(articleTokens[index].tokens, (token, bottomCallback) => {
-				const weight = tfidf.tfidf(token[0], index);
-				// if (articleTokens[index].article.title.includes("Israel"))
-				// 	console.log(
-				// 		articleTokens[index].article.title,
-				// 		token,
-				// 		weight
-				// 	);
-
-				if (weight > 70 && token[1] == "noun") tokenWeights[token[0]] = weight
-				else if (weight > 30 && token[1] == "rest") tokenWeights[token[0]] = weight
-				bottomCallback();
-			}, (err) => {
-				let weightedTokens = genWeightedTokens(tokenWeights);
-				weightedTokens = weightedTokens.map((token) => token.toLowerCase());
-				articleTokens[index].article.tokens = weightedTokens;
-				articleTokens[index].article.markModified("tokens");
-				articleTokens[index].article.save();
-
-				topCallback();
-			})
-			
 		}, (err) => {
 			setTimeout(() => {
-		      // in case any more mongodb commands are running
-		      console.log("DONE");
-		      process.exit(0)
-		    }, 1000);
+				console.log("DONE");
+				process.exit(0);
+			}, 1000)
 		});
 	});
+	
 
 }
 
