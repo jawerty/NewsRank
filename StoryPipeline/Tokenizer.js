c7e3705bccc14417407afb222134769bb55eb92d
Jared Wright <jared@jareds-mbp.home>
2018-08-10 09:51:45 -0400

    tokenizer fix

--- StoryPipeline/Tokenizer.js
+++ StoryPipeline/Tokenizer.js
@@ -114,8 +114,8 @@ const Tokenizer = () => {
 						// }
 							
 
-						if (weight > 70 && token[1] == "noun") tokenWeights[token[0]] = weight
-						else if (weight > 30 && token[1] == "rest") tokenWeights[token[0]] = weight
+						if (weight > 20 && token[1] == "noun") tokenWeights[token[0]] = weight
+						else if (weight > 20 && token[1] == "rest") tokenWeights[token[0]] = weight
 						bottomCallback();
 					}, (err) => {
 						let weightedTokens = genWeightedTokens(tokenWeights);
