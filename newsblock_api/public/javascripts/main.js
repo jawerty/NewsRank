dd766ec41aad3e0c65d136b08c0b54954ccbffdb
Jared Wright <jared@Jareds-MacBook-Pro.local>
2018-08-14 00:59:43 -0400

    recent article

--- newsblock_api/public/javascripts/main.js
+++ newsblock_api/public/javascripts/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 	$(".how-this-works-link").click(function() {
-		ga('send', 'event', 'How This Works', 'Click', publication);
+		ga('send', 'event', 'How This Works', 'Click');
 	    $([document.documentElement, document.body]).animate({
 	        scrollTop: $("#how-this-works").offset().top
 	    }, 750);
 	});	
 
 	$('.ranking-row').click(function() {
-		publication = $(this).find('.publication').text();
+		const publication = $(this).find('.publication').text();
 		const articleRow = $(this).next();
 		ga('send', 'event', 'Rankings', 'Click', publication);
 		articleRow.find("div").slideToggle();
